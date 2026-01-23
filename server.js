10870777a40a8f17dfd803f0935dc70e3486c291
HildaWadada <wadadahilda@gmail.com>
2026-01-23 17:19:35 +0300

    updated

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const path = require('path');
-// const contactmeRoutes = require('./routes/contactmeRoutes');
-require('dotenv').config();
 
 const app = express();
 
@@ -9,19 +7,17 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-// Static files
-app.use(express.static(path.join(__dirname, 'public')));
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// // Contact form route
-// app.use('/', contactmeRoutes);
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Catch-all for SPA (Express 5 safe)
-app.use((req, res) => {
+// Home route
+app.get('/', (req, res) => {
   res.render('portfolio', { formMessage: null });
 });
 
-// Start server
-const PORT = process.env.PORT || 3003;
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+// ❌ NO app.listen on Vercel
+module.exports = app;
