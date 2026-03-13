86acaf28289c5c852a7c376d351d3cd436e77bb2
HildaWadada <wadadahilda@gmail.com>
2026-03-13 10:36:14 +0300

    fix build

--- hilda-portfolio/vite.config.js
+++ hilda-portfolio/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react:  ['react', 'react-dom'],
+          icons:  ['react-icons', 'react-feather'],
+          emailjs: ['@emailjs/browser'],
+        },
+      },
+    },
+  },
 })
